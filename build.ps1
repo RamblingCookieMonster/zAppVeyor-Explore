e5f72e572c60341ba8594c2075a203b2e0ad3113
RamblingCookieMonster <ramblingcookiemonster@users.noreply.github.com>
2016-10-04 23:01:57 -0400

    Add newlines, force BH install

--- build.ps1
+++ build.ps1
@@ -2,33 +2,32 @@
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 # Some CI/CD helper functions, set up some env variables
-Install-Module BuildHelpers
+Install-Module BuildHelpers -Force
 Import-Module BuildHelpers
 Set-BuildEnvironment
 
 # Display some details about the environment!
 $lines = '----------------------------------------------------------------------'
 
-"$lines`PSVERSIONTABLE`n`n"
+"$lines`nPSVERSIONTABLE`n`n"
 $PSVersionTable
 
-"`n`n$lines`BUILDHELPERS`n`n"
+"`n`n$lines`nBUILDHELPERS`n`n"
 Get-Item ENV:BH*
 
-"`n`n$lines`MODULES`n`n"
+"`n`n$lines`nMODULES`n`n"
 Get-Modules -ListAvailable |
     Select Name, Version, Path |
     Sort Name
 
-"`n`n$linesENV`n`n"
+"`n`n$lines`nENV`n`n"
 Get-ChildItem ENV:
 
-"`n`n$lines`PSMODULEPATH`n`n"
+"`n`n$lines`nPSMODULEPATH`n`n"
 $ENV:PSModulePath -Split ";"
 
-"`n`n$lines`PATH`n`n"
+"`n`n$lines`nPATH`n`n"
 $ENV:PATH -Split ";"
 
-"`n`n$lines`VARIABLES`n`n"
+"`n`n$lines`nVARIABLES`n`n"
 Get-Variable
-
