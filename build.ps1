7d0de665909a8f26697474912a70f84b2a037ab1
Warren Frame <RamblingCookieMonster@users.noreply.github.com>
2017-04-06 01:49:55 -0400

    bump

--- build.ps1
+++ build.ps1
@@ -6,35 +6,5 @@ Install-Module BuildHelpers -Force
 Import-Module BuildHelpers
 Set-BuildEnvironment
 
-# Display some details about the environment!
-$lines = '----------------------------------------------------------------------'
-
-"$lines`nPSVERSIONTABLE`n`n"
-$PSVersionTable |
-    Out-Host
-
-"`n`n$lines`nBUILDHELPERS`n`n"
-Get-Item ENV:BH* |
-    Out-Host
-
-"`n`n$lines`nMODULES`n`n"
-Get-Module -ListAvailable |
-    Select Name, Version, Path |
-    Sort Name |
-    Out-Host
-
-"`n`n$lines`nENV`n`n"
-Get-ChildItem ENV: |
-    Out-Host
-
-"`n`n$lines`nPSMODULEPATH`n`n"
-$ENV:PSModulePath -Split ";" |
-    Out-Host
-
-"`n`n$lines`nPATH`n`n"
-$ENV:PATH -Split ";" |
-    Out-Host
-
-"`n`n$lines`nVARIABLES`n`n"
-Get-Variable |
-    Out-Host
+# Look around
+Get-BuildEnvironmentDetail -KillKittens
